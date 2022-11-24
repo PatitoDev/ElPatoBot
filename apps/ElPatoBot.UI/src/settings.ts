a1d2c55ae6bb4673655bf1b6177c828b270a93b8
Niv3kElPato <niv3k.business@gmail.com>
2022-11-24 23:35:56 +0000

    ok

--- apps/ElPatoBot.UI/src/settings.ts
+++ apps/ElPatoBot.UI/src/settings.ts
@@ -1,6 +1,8 @@
+const baseUrl = 'elpatobotserverdev-env.eba-rqe5gg35.eu-west-2.elasticbeanstalk.com/';
+
 const settings = {
-    serverUrl: 'http://localhost:8084/',
-    websocketUrl: 'ws://localhost:8084'
+    serverUrl: `http://${baseUrl}`,
+    websocketUrl: `ws://${baseUrl}`
 }
 
 export default settings;
